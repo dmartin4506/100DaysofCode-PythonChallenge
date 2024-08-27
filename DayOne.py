6bf358be34f23eb180a32cb46a994635b082702f
Dylan Martin <85652641+dmartin4506@users.noreply.github.com>
2024-08-26 20:44:18 -0400

    Create DayOne.py
    Day one of Coding Challenge, Band Name Generator Program

---
+++ DayOne.py
@@ -0,0 +1,5 @@
+print("Hello everyone, welcome to my Band Name Generator Program")
+city = input("What is the name of the City you grew up in?")
+pet = input("What is the name of one of your pets?")
+
+print("Your band name could be " + city + pet)
